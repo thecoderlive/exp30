dff07920c52a33bbc771df0025f1a65cc84a318f
The Coder <82015917+thecoderlive@users.noreply.github.com>
2022-11-13 03:36:58 +0530

    Initial Commit for file: DATA.js

---
+++ DATA.js
@@ -0,0 +1,145 @@
+export const item = {
+    "id": "2bfa5129-e8a2-4146-853a-e83f07a83274",
+    "home_feed": [
+        {
+            "id": "f99c8175-3612-4f9c-b31b-b4c3f9731f11",
+            "artist_name": "act",
+            "home_carousel": [
+                {
+                    "id": "9ca9240e-3898-4431-92e8-bd8729c028bc",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/194.png"
+                },
+                {
+                    "id": "49955bb6-f5a3-44b0-8cc3-5a63d8019bca",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/14.png"
+                },
+                {
+                    "id": "fff5fa54-5d6a-4bba-a149-c5cdb4f7bb98",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                },
+                {
+                    "id": "3e0bc5df-d3b4-464d-879b-0ab5571b701c",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/138.png"
+                },
+                {
+                    "id": "330ceb03-53b6-4235-9dcd-06baf8a9c2c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                }
+            ],
+            "art_caption": "be",
+            "art_description": "Career ground once alone. Summer fall speech difference money six. News mission at visit six."
+        },
+        {
+            "id": "05c63674-caf0-4288-b6ea-467e688b925b",
+            "artist_name": "single",
+            "home_carousel": [
+                {
+                    "id": "de8dc843-275b-4e75-879d-95a65793ca26",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/28.png"
+                },
+                {
+                    "id": "1876393a-8e92-42b9-933c-a99a1549c485",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/305.png"
+                },
+                {
+                    "id": "8ce2f174-40bb-4166-af8f-01b35d83b19d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/218.png"
+                },
+                {
+                    "id": "79372230-a3b3-471e-a09f-0c1991d77e60",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/98.png"
+                },
+                {
+                    "id": "4a3a157a-c7d8-4fb1-86d9-2b05ca5cae9d",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/60.png"
+                }
+            ],
+            "art_caption": "leave",
+            "art_description": "Rise above throughout fear goal official event."
+        },
+        {
+            "id": "737afe8f-22f1-4161-a9b8-a79e6bb20f22",
+            "artist_name": "give",
+            "home_carousel": [
+                {
+                    "id": "0d1d8531-f750-492e-9789-ff3007ad287b",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/366.png"
+                },
+                {
+                    "id": "76f32317-edac-4872-8592-fd53c33f9846",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/356.png"
+                },
+                {
+                    "id": "1338b5f6-ec5e-41a1-bc11-e355af5310c3",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/80.png"
+                },
+                {
+                    "id": "15413384-c740-41f2-be67-18df1a5961bb",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/348.png"
+                },
+                {
+                    "id": "4233cb72-68de-439f-97ac-c161f4eed544",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/277.png"
+                }
+            ],
+            "art_caption": "night",
+            "art_description": "Truth effort collection away push road."
+        },
+        {
+            "id": "06a0faa2-0d34-4ed4-907a-671a5f5898b2",
+            "artist_name": "serious",
+            "home_carousel": [
+                {
+                    "id": "a185e50b-50cb-454d-aa5a-5f65a8fd3159",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/4.png"
+                },
+                {
+                    "id": "261a0828-abdc-4317-9b79-61a18b600de6",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/42.png"
+                },
+                {
+                    "id": "88166840-e1c6-40d3-8ac0-1c516592e242",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/367.png"
+                },
+                {
+                    "id": "44c37cb3-f51f-4434-8b69-a15b53b60fe1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/108.png"
+                },
+                {
+                    "id": "ee84bd4b-32b4-42e2-9435-7698899021c1",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/46.png"
+                }
+            ],
+            "art_caption": "listen",
+            "art_description": "Sort relationship culture continue level some. Without risk style."
+        },
+        {
+            "id": "c4cd9450-db10-42c5-a441-769b8c1b3574",
+            "artist_name": "use",
+            "home_carousel": [
+                {
+                    "id": "26f48a68-41aa-4d10-a409-d5bd495522d8",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/23.png"
+                },
+                {
+                    "id": "22d73648-7a4d-4ac5-b0d0-6b4f97bcd0be",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/188.png"
+                },
+                {
+                    "id": "bb41c481-d2ca-4805-b74a-ec46233ef1f4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/112.png"
+                },
+                {
+                    "id": "3f8c92dc-f791-4a6d-8f62-bc8b0a8a8516",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/151.png"
+                },
+                {
+                    "id": "25d46f2b-d3ad-4eb6-bff8-cdccc6624db4",
+                    "art_photo": "https://the-coder.s3.ap-south-1.amazonaws.com/images_540/116.png"
+                }
+            ],
+            "art_caption": "almost",
+            "art_description": "Various federal next road financial food right course. Power pass than space drug. Arrive computer natural make food get."
+        }
+    ]
+}
